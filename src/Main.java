11f2b466d7487106a519100e107d2184eed66b95
Alex <alexdanmoldovan1@gmail.com>
2019-09-16 22:03:39 +0300

    tema

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args) {
+    //ex 1.
+        // System.out.println("Hello\nAlex");
+    /*ex 2.
+        int a = 2120;
+        int b = 120;
+        int sum = 0;
+        sum = a + b;
+        System.out.println(sum);
+    */
+    /*ex 3.
+        float a = 10;
+        float b = 3;
+        float cat ;
+        cat = a / b;
+        System.out.println(cat);
+    */
+    /*ex 4.a.
+        int a = -5;
+        int b = 8;
+        int c = 6;
+        int rez = 0;
+        rez = a + (b * c);
+        System.out.println(rez);
+    */
+    /*ex 4.b.
+        int a = 55;
+        int b = 9;
+        int rez;
+        rez = (a + b) % b;
+        System.out.println(rez);
+     */
+    /*ex 4.c.
+        float a = 20;
+        float b = -3;
+        float c = 5;
+        float d = 8;
+        float rez;
+        rez = a + ((b * c) / d);
+        System.out.println(rez);
+     */
+    //ex 4.d.
+        System.out.println((5 + ((15 / 3) * 2) - 8) % 3);
+    }
+}
